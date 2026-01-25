2d76be91f64e3c8766a981e4a85e13be0bcff983
Anish Phatake <anishphatake8055.2@gmail.com>
2026-01-25 09:59:09 +0530

    spell correction, new file add

--- largest_among3.py
+++ largest_among3.py
@@ -3,7 +3,7 @@ n2=float(input("enter number2: "))
 n3=float(input("enter number3: "))
 
 if n1>n2 and n1>n3:
-    print("first number is grater.")
+    print("first number is greater.")
 elif n2>n1 and n2>n3:
     print("second number is greater.")
 else:
