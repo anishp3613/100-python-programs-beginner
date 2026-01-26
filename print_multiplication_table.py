b3f1f6814e67faaa27046bb9cc0b2557d507e164
Anish Phatake <anishphatake8055.2@gmail.com>
2026-01-26 13:35:08 +0530

    table using for and while loop

--- print_multiplication_table.py
+++ print_multiplication_table.py
@@ -1,3 +1,12 @@
+# table printing using for loop
 n=int(input("enter number: "))
 for i in range(1,11):
     print(n,"x",i,"=",n*i)
+
+#table printing using while loop
+
+n=int(input("enter number : "))
+i=1
+while i<=10:
+    print(n*i)
+    i+=1
