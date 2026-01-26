e93ea903a519022ac04a047b09307e3d36851fd2
Anish Phatake <anishphatake8055.2@gmail.com>
2026-01-26 13:25:29 +0530

    print multiplication table

--- factorial_of_number.py
+++ factorial_of_number.py
@@ -1,6 +1,18 @@
+# factorial using for loop
+
 n=int(input("enter number : "))
 
 fact=1
 for i in range(1,n+1):
         fact*=i
 print(fact)
+
+# factorial using recursion
+
+def fact(a):
+        if a==0:
+                return 1
+        else:
+                return ((a)*fact(a-1))
+num=int(input("enter number: "))
+result=fact(num)
